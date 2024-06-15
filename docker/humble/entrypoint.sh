2bfc22a9d1574ffc0ac91ebc953d4d1706a96623
Kim Namu (Kim Nam Woo) <pre3ice@gmail.com>
2024-06-16 07:20:36 +0900

    Update entrypoint.sh

--- docker/humble/entrypoint.sh
+++ docker/humble/entrypoint.sh
@@ -355,14 +355,14 @@ exec /bin/tini -- supervisord -n -c /etc/supervisor/supervisord.conf
 
 workingspace=colcon_ws
 
-mkdir -p ~/${workingspace}/src
+mkdir -p ~/${workingspace}/src && cd ~/${workingspace}/src
 git clone -b main-ros2 https://github.com/Unity-Technologies/ROS-TCP-Endpoint
-cd ~/${workingspace} colcon build --symlink-install --packages-select ros_tcp_endpoint
+cd ~/${workingspace} && colcon build --symlink-install --packages-select ros_tcp_endpoint
 
 source ~/${workingspace}/install/setup.bash
 
-mkdir -p ~/${workingspace}/src
+cd ~/${workingspace}/src
 git clone https://github.com/namwoo/unity_slam_example
-cd ~/${workingspace} colcon build --symlink-install --packages-select unity_slam_example
+cd ~/${workingspace} && colcon build --symlink-install
 
 source ~/${workingspace}/install/setup.bash
