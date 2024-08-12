9b91c3d83d86d15cd8716c0d460d7374d6c071dc
NamWoo <pre3ice@gmail.com>
2024-08-12 12:37:26 +0900

    add mirobot ros2

--- docker/humble/ros2build.sh
+++ docker/humble/ros2build.sh
@@ -17,7 +17,21 @@ git clone https://github.com/namwoo/unity_slam_example
 cd ~/${workingspace} && colcon build --symlink-install --packages-select unity_slam_example
 source ~/${workingspace}/install/setup.bash
 
-#
+sudo apt-get install -y ros-humble-jointstate-publisher-gui
+
+#ros2 serial pakage
+cd ~/${workingspace}/src
+git clone https://github.com/RoverRobotics-forks/serial-ros2.git
+cd ~/${workingspace} && colcon build --symlink-install --cmake-args -DCMAKE_BUILD_TYPE=Release serial
+source ~/${workingspace}/install/setup.bash
+
+#mirobot
+cd ~/${workingspace}/src
+git clone https://github.com/Road-Balance/mirobot_ros2
+cd ~/${workingspace} && colcon build --symlink-install --packages-select mirobot_description
+source ~/${workingspace}/install/setup.bash
+
+
 #workingspace=colcon_ws
 echo 'export TURTLEBOT3_MODEL=waffle_pi' >> ~/.bashrc
 echo 'source ~/${workingspace}/install/setup.bash' >> ~/.bashrc
